b2a24011db5ac862308e82b9d704f6ccc7a58f42
GouzGouz <me@gmail.fr>
2024-11-29 10:22:01 +0100

    parse to the sym table

--- src/main.c
+++ src/main.c
@@ -18,10 +18,42 @@ bool is_elf(Elf64_Ehdr *header)
 }
 
 
-void parse_elf64(Elf64_Ehdr *header){
+void parse_elf64(void *file_map){
+	Elf64_Ehdr *header = (Elf64_Ehdr *)file_map;
+	Elf64_Shdr *section_headers = (Elf64_Shdr *)(file_map + header->e_shoff);
+	char *shstrtab = file_map + section_headers[header->e_shstrndx].sh_offset;
+
+	Elf64_Shdr *symtab = NULL;
+	Elf64_Shdr *strtab = NULL;
 
 
+	for (int i = 0; i < header->e_shnum; i++) {
+		Elf64_Shdr *sh_i = &section_headers[i];//get the section header at index i
+		char *section_name = shstrtab + sh_i->sh_name; // like doing tab[index] to get the name of the section
+		// ft_putstr_fd(section_name, 1);
+		// ft_putstr_fd("\n", 1);
+		if (ft_strncmp(section_name, ".symtab", 8) == 0)
+			symtab = sh_i;
+		if (ft_strncmp(section_name, ".strtab",8) == 0)
+			strtab = sh_i;
+	}
 
+	//from here we have the symbol table and the string table
+	//we can now iterate through the symbol table using the same logic from the prev loop
+
+	//Get the symbol table and the string table
+	Elf64_Sym *symbols = (Elf64_Sym *)(file_map + symtab->sh_offset);
+	char *strtab_content = file_map + strtab->sh_offset;
+	int symbol_count = symtab->sh_size / symtab->sh_entsize;
+	for (int i = 0; i < symbol_count; i++) {
+		Elf64_Sym *sym = &symbols[i];
+		char *sym_name = strtab_content + sym->st_name;
+		int type = ELF64_ST_TYPE(sym->st_info); //function obj ...
+		int bind = ELF64_ST_BIND(sym->st_info); //local or global or "weak"
+		// i have no idea how i can get the value of the each symbol ("T", "U", etc...)
+		// if (sym->st_shndx == )
+
+		// }
 }
 
 void parse_elf32(void *file_map){
@@ -62,7 +94,7 @@ void start_parsing(char *file)
 	if (is_elf(file_map)){
 		if (header->e_ident[EI_CLASS] == ELFCLASS64)
 		{
-			parse_elf64(header);
+			parse_elf64(file_map);
 		}
 		else if (header->e_ident[EI_CLASS] == ELFCLASS32)
 		{
